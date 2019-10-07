d9ffb3392fd27db04fb5e8f9639c3ff6a324542b
Ryan Borja <rborja@prospecttrax.com>
2019-10-07 11:42:51 -0400

    chagned constants file to include abbreviation

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,28 +4,36 @@ import { bootStrap3 } from './bootstrap3/bootstrap3';
 export const constants = {
 
     blocks: {
+
         gridsystem: { 
             container: {id: 'container_block'},
             row: { id: 'row_block'},
             column: { id: 'column_block' },
         },
+
         typography: {
             heading: { id: 'heading_block'},
             paragraph: { id: 'paragraph_block'},
+            abbreviation: { id: 'abbvreviation_block' },
         },
+
     },
 
     components: {
+
         gridsystem: { 
             container: {id: 'container_component'},
             row: { id: 'row_component'},
             column: { id: 'column_component' },
         },
+        
         typography: {
+
             heading: { 
                 id: 'heading_component',
                 tags: bootStrap3.typography.headings.tags,
             },
+
             paragraph: { 
                 id: 'paragraph_component',
                 tags: ['p'],
@@ -34,7 +42,15 @@ export const constants = {
                     ...bootStrap3.typography.alignment.classes,
                     ...bootStrap3.typography.trasnformation.classes,
                 ],
+            },
+
+            abbreviation: {
+                id: 'abbreviation_component',
+                tags: ['abbr'],
+                classes: ['initialism'],
+                attributes: ['title'],
             }
+            
         },
     },
 
