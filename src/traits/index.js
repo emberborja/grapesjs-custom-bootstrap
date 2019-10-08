f15635ed36846e2a72d31f431dc83234915a888b
Ryan Borja <rborja@prospecttrax.com>
2019-10-08 10:08:19 -0400

    got rid of unused trait class for headings

--- src/traits/index.js
+++ src/traits/index.js
@@ -1,7 +1,5 @@
 import selectClass from './selectClass/'
-import selectHeadingTag from './selectHeadingTag/'
 
 export default function (editor, config = {}) {
   selectClass(editor, config)
-  selectHeadingTag(editor, config)
 }
